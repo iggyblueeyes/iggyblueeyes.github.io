9e28e0cf7a5901b28c4360aac02729e007f71f2a
calebjignace <cignace@asu.edu>
2022-01-31 23:45:51 -0700

    update

---
+++ default-html.py
@@ -0,0 +1,16 @@
+"""
+This file will allow me to update all the html files at the same time.
+"""
+
+import re
+import os
+
+folders = [e for e in os.listdir() if "." not in e]
+htmlFiles = [e for e in os.listdir() if ".html" in e]
+
+print(folders)
+print(htmlFiles)
+
+
+
+#os.chdir("tutoring")
