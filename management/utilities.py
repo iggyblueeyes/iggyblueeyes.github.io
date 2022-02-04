fcbc903468bc6103170a4d23f216b8c45c43a675
calebjignace <cignace@asu.edu>
2022-02-04 15:45:06 -0700

    Update

--- management/utilities.py
+++ management/utilities.py
@@ -7,6 +7,8 @@ import os
 def getDirectoriesInside(path, directoryPaths = []):
     if (path[-1] != '/'):
         path += '/'
+    if len(directoryPaths):
+        directoryPaths.append(path);
     directoriesInside = [x for x in os.listdir(path) if not any((c in '._') for c in x)]
     if (len(directoriesInside) > 0):
         for directory in directoriesInside:
