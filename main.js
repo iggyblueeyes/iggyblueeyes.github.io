37f5097d477c327cac30b0826f16a9caea36b425
calebjignace <cignace@asu.edu>
2022-02-08 09:03:46 -0700

    Update

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // pass
 
-function setNav(id, url) {
-    document.getElementById(id).onclick = function() {
-	window.open(url);
+function resize() {
+    if (window.innerWidth < 500) {
+        document.body.style.width = String(0.90*window.innerWidth) + "px"; 
+    } else {
+	document.body.style.width = String(0.6*window.innerWidth) + "px";
     }
+    console.log(document.body.style.width)
 }
 
-setNav("home", "https://iggyblueeyes.github.io")
+window.addEventListener("resize", resize)
+
+resize()
