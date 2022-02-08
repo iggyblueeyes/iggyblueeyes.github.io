00e811fb7f59669a2821e9435105ae17c2fb66f7
calebjignace <cignace@asu.edu>
2022-02-07 17:58:15 -0700

    Update

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // pass
 
 function setNav(id, url) {
-    document.getElementById(id).onclick = window.location = url;
+    document.getElementById(id).onclick = function() {
+	window.location = url;
+    }
 }
 
-setNav("home", "https://iggyblueeyes/github.io")
+setNav("home", "https://iggyblueeyes.github.io")
